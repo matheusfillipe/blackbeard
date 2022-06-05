b1894b65cee1003117339ab1a602b914df4a61cd
matheusfillipe <matheusfillipeag@gmail.com>
2022-06-05 02:31:22 -0300

    Ci workflow

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
+# First argument should be platform (windows, linux, etc)
+# Next arguments are the architetures to build to
+# List architetures with `go tool dist list | grep linux`
 
 version=$(git describe --tags --abbrev=0)
 version=${version/[a-zA-Z]/}
 
-archs=(amd64 arm64 386)
+platform=$1
+shift
+archs=$@
 
+mkdir -p build
+
+echo "Platform $platform"
 for arch in ${archs[@]}
 do
-    env GOOS=$1 GOARCH=${arch} CGO_ENABLED=1 go build -ldflags="-X 'main.Version=$version' -X 'main.BuildDate=$(date -u)'" -o prepnode_${arch}
-
+    echo "  Building $arch"
+    mkdir $arch
+    env GOOS=${platform} GOARCH=${arch} CGO_ENABLED=1 go build -ldflags="-X 'main.Version=$version' -X 'main.BuildDate=$(date -u)'" -o "build/$arch/blackbeard"
 done
