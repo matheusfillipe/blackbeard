84b241bc1063a408b93589e240d7ddb3e14f9dfb
matheusfillipe <matheusfillipeag@gmail.com>
2022-06-05 01:24:10 -0300

    fixing ci

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,5 +3,10 @@
 version=$(git describe --tags --abbrev=0)
 version=${version/[a-zA-Z]/}
 
+archs=(amd64 arm64 386)
 
-go build -ldflags="-X 'main.Version=$version' -X 'main.BuildDate=$(date -u)'"
+for arch in ${archs[@]}
+do
+    env GOOS=$1 GOARCH=${arch} CGO_ENABLED=1 go build -ldflags="-X 'main.Version=$version' -X 'main.BuildDate=$(date -u)'" -o prepnode_${arch}
+
+done
