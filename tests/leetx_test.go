2ea6752a5f521a5dc53cde00e602f96c9c8a9e97
matheusfillipe <matheusfillipeag@gmail.com>
2022-07-26 03:50:28 -0300

    Ignore test

--- tests/leetx_test.go
+++ tests/leetx_test.go
@@ -1,17 +1,18 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"github.com/matheusfillipe/blackbeard/providers"
-// 	"testing"
-// )
+import (
+	"fmt"
+	"github.com/matheusfillipe/blackbeard/providers"
+	"testing"
+)
 
-// func TestLeetx(t *testing.T) {
-// 	t.Run("leetx test", func(t *testing.T) {
-// 		a := providers.GetProviders()["1337x"]
-// 		shows := a.SearchShows("attack on titan")
-// 		fmt.Println("Shows: ", shows[0])
-// 		episodes := a.GetEpisodes(&shows[0])
-// 		fmt.Println(episodes)
-// 	})
-// }
+func TestLeetx(t *testing.T) {
+	t.Run("leetx test", func(t *testing.T) {
+		return
+		a := providers.GetProviders()["1337x"]
+		shows := a.SearchShows("attack on titan")
+		fmt.Println("Shows: ", shows[0])
+		episodes := a.GetEpisodes(&shows[0])
+		fmt.Println(episodes)
+	})
+}
